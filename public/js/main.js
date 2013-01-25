4df5b5ea5278e4d4334b11c883ceb4251757fe67
Brendan Nee <brendan.nee@gmail.com>
2013-01-24 22:54:52 -0800

    js MUNI times

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
+jQuery.fn.orderBy = function(keySelector) {
+    return this.sort(function(a,b)
+    {
+        a = keySelector.apply(a);
+        b = keySelector.apply(b);
+        if (a > b)
+            return 1;
+        if (a < b)
+            return -1;
+        return 0;
+    });
+};
+
 function updateClock() {
   var currentTime = new Date(),
       currentHours = currentTime.getHours(),
@@ -22,35 +35,35 @@ function getMUNI(){
   var MUNIroutes = [
   {
     route: 5,
-    stop: 15390
+    stop: 5390
   },
   {
     route: 6,
-    stop: 14951
+    stop: 4951
   },
   {
     route: 21,
-    stop: 14992
+    stop: 4992
   },
   {
     route: 22,
-    stop: 14632
+    stop: 4632
   },
   {
     route: 71,
-    stop: 14951
+    stop: 4951
   },
   {
     route: '71L',
-    stop: 14951
+    stop: 4951
   },
   {
     route: 'N',
-    stop: 17318
+    stop: 7318
   },
   {
     route: 'N OWL',
-    stop: 14951
+    stop: 4951
   },
   ];
   
@@ -68,29 +81,44 @@ function getMUNI(){
       },
       dataType: 'xml',
       success:function(result){
-        var div = $('#muni' + route.toString().replace(/\s/g, '') + '_' + stop);
-        
-        //Clear old times
-        $('.times', div).html('');
+        var divName = 'muni' + route.toString().replace(/\s/g, '') + '_' + stop;
+        var div = $('#' + divName);
+
+        if(!div.length) {
+          var div = $('<div>')
+            .addClass('muni')
+            .attr('id', divName)
+            .append($('<div>')
+              .addClass('busnumber')
+              .html(route.toString()))
+            .append($('<div>')
+              .addClass('nextbus time'))
+            .append($('<div>')
+              .addClass('laterbuses')
+              .append($('<span>')
+                .addClass('time'))
+              .append($('<span>')
+                .addClass('time')))
+            .appendTo('#times');
+        } else {
+          //Clear old times
+          $('.time', div).html('');
+        }
         
         //Check if route is still running
-        if($(result).find('prediction').length > 0){
-          div.show();
-          
-          var count = 0;
-          
-          $(result).find('prediction').each(function(i, data){
-            //Limit to 3 results, only show times less than 100, don't show results that are 0
-            if(count < 3 && $(data).attr('minutes') < 100 && $(data).attr('minutes') > 0){
-              
-              $('.times', div).append('<span>' + $(data).attr('minutes') + '</span>');
-              
-              count++;
-            }
-          });
-        } else {
-          div.hide();
-        }
+        div.toggle($(result).find('prediction').length > 0)
+        
+        var idx = 0;
+        $(result).find('prediction').each(function(i, data){
+          //Limit to 3 results, only show times less than 100, don't show results that are 0
+          if(idx < 3 && $(data).attr('minutes') < 100 && $(data).attr('minutes') > 0){
+            console.log(idx)
+            console.log($(data).attr('minutes'))
+            $('.time', div).eq(idx).html($(data).attr('minutes'));
+            
+            idx++;
+          }
+        });
       }
     });
   }
