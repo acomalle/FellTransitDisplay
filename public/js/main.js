d57f4dbb13f29f5f80dce86cbadf175c5efd39f4
Joakim Keussen <joakim.keussen@gmail.com>
2013-01-24 22:33:40 -0800

    Styling bus routes and fixing the nearby section's height

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 function updateClock() {
   var currentTime = new Date(),
       currentHours = currentTime.getHours(),
@@ -8,18 +7,9 @@ function updateClock() {
   // Pad the minutes and seconds with leading zeros, if required
   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
   currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-  // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
+  
   // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  var currentTimeString = currentHours + ":" + currentMinutes;
 
   // Update the time display
   $('#clock').html(currentTimeString);
